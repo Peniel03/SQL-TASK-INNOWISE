46811d57c7a723d151894a1bed9481c553ce2cea
Peniel03 <70961783+Peniel03@users.noreply.github.com>
2022-11-10 19:07:33 +0300

    Update FirstQuery.sql

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -1,5 +1,5 @@
 --------------------------------TABLE CREATION QUERIES-------------------------------------
----------------Accounts TABLE CREATION---------------
+
 
 USE Bank ;
 CREATE TABLE Accounts
@@ -9,8 +9,7 @@ Account_Number_Of_Cards BIGINT NULL,
 Account_Balance MONEY NULL,
  );
  ---------------------------------------------------------------------
- ---------------Banks TABLE CREATION---------------------
- USE Bank ;
+ 
 CREATE TABLE Banks
 (Bank_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Banks PRIMARY KEY,
  Bank_Name VARCHAR(100) NULL,
@@ -20,8 +19,7 @@ CREATE TABLE Banks
  Bank_Contact VARCHAR(100) NULL,
  );
 ------------------------------------------------------------------------
-----------------Cards TABLE CREATION------------------------
-USE Bank ;
+ 
 CREATE TABLE Cards
 (Card_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Cards PRIMARY KEY,
 Card_Number  VARCHAR(100) NULL,
@@ -29,8 +27,7 @@ Card_Balance MONEY NULL,
 Account_Id BIGINT NULL,
 );
 --------------------------------------------------------------------------
------------------Clients TABLE CREATION---------------------
-USE Bank ;
+ 
 CREATE TABLE Clients
 (Client_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Clients PRIMARY KEY,
 Client_Name VARCHAR(100) NULL,
@@ -40,22 +37,19 @@ Social_Status_Id BIGINT NULL,
 Bank_Id BIGINT NULL,
 );
 -----------------------------------------------------------------------------
-------------------SocialStatus TABLE CREATION---------------------
-USE Bank ;
+ 
 CREATE TABLE SocialStatus
 (SocialStatus_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_SocialStatus PRIMARY KEY,
  SocialStatus_Number BIGINT NULL,
  SocialStatus_Name VARCHAR(100) NULL,
  );
 --------------------------------------------------------------------------------
-------------------Subsidiaries TABLE CREATION----------------------
-USE Bank ;
-CREATE TABLE Subsidiaries
+ 
+ CREATE TABLE Subsidiaries
 (Subsidiary_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Subsidiaries PRIMARY KEY,
  Subsidiary_Name VARCHAR(100) NULL,
  Subsidiary_City VARCHAR(100) NULL,
  Subsidiary_Address VARCHAR(100) NULL,
  Bank_Id BIGINT NULL,
  );
-----------------------------------------------------------------------------------
---------------------------------------------------------------------------
+ 

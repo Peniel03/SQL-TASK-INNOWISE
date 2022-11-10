ca9a15c427c1b8838fec4b46f55914b58e8663a0
Peniel03 <70961783+Peniel03@users.noreply.github.com>
2022-11-10 19:12:14 +0300

    Update FirstQuery.sql

--- FirstQuery.sql
+++ FirstQuery.sql
@@ -1,5 +1,3 @@
---------------------------------TABLE CREATION QUERIES-------------------------------------
-
 
 USE Bank ;
 CREATE TABLE Accounts
@@ -8,8 +6,8 @@ Account_Type VARCHAR(100) NULL,
 Account_Number_Of_Cards BIGINT NULL,
 Account_Balance MONEY NULL,
  );
- ---------------------------------------------------------------------
- 
+
+
 CREATE TABLE Banks
 (Bank_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Banks PRIMARY KEY,
  Bank_Name VARCHAR(100) NULL,
@@ -18,15 +16,16 @@ CREATE TABLE Banks
  Bank_City VARCHAR(100) NULL,
  Bank_Contact VARCHAR(100) NULL,
  );
-------------------------------------------------------------------------
- 
+
+
+
 CREATE TABLE Cards
 (Card_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Cards PRIMARY KEY,
 Card_Number  VARCHAR(100) NULL,
 Card_Balance MONEY NULL,
 Account_Id BIGINT NULL,
 );
---------------------------------------------------------------------------
+ 
  
 CREATE TABLE Clients
 (Client_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Clients PRIMARY KEY,
@@ -36,14 +35,15 @@ Client_Phone_Number VARCHAR(100) NULL,
 Social_Status_Id BIGINT NULL,
 Bank_Id BIGINT NULL,
 );
------------------------------------------------------------------------------
- 
+
+
 CREATE TABLE SocialStatus
 (SocialStatus_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_SocialStatus PRIMARY KEY,
  SocialStatus_Number BIGINT NULL,
  SocialStatus_Name VARCHAR(100) NULL,
  );
---------------------------------------------------------------------------------
+
+
  
  CREATE TABLE Subsidiaries
 (Subsidiary_Id BIGINT IDENTITY(1,1) CONSTRAINT PK_Subsidiaries PRIMARY KEY,
